32889e30ae497baf66d1a25aed25b9b3bae603b8
mehalamani <Smehala6789@gmail.com>
2016-06-06 11:34:14 +0530

    Create n.c

---
+++ n.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n==0)
+		printf("Given number is zero\n");
+	else if(n>0)
+		printf("Given number is positive\n");
+	else
+		printf("Given number is negative\n");
+	// your code goes here
+	return 0;
+}
